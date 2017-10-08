c357b4eaedde0c83b956733f0cd6851cae80088b
Finalgalaxy <finalgalaxy@gmail.com>
2017-10-08 16:31:54 +0200

    Fixed typos.

--- electron/gulpfile.js
+++ electron/gulpfile.js
@@ -5,18 +5,18 @@ const del = require('del');
 const runSeq = require('run-sequence');
 
 gulp.task('copy_package_json', function() {
-    gutil.log("Copying package*.json files inside src folder...")
+    gutil.log("Copying package*.json files inside src folder for build purposes...")
     return gulp.src('./package*.json').pipe(gulp.dest('./src'));
 });
 
 gulp.task('actual_build', function(){
-    gutil.log("Invoking 'npm run build'...")
+    gutil.log("Building Electron application...")
     return gulp.src('./').pipe(gexec('npm run electron-packager-build'))
                     .pipe(gexec.reporter({err:true, stderr:true, stdout:true}));
 });
 
 gulp.task('clean_package_json', function(){
-    gutil.log("Deleting...");
+    gutil.log("Cleaning package.json transferred inside /src for build purposes...");
     return del('./src/package*.json').then(gutil.log("Ok!"));
 });
 
